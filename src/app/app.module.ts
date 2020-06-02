d9793023a1a430c9c89b7b797856d0f3a9189743
DavidKim01 <dk9100@gmail.com>
2020-06-02 00:16:28 -0400

    added highcharts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HighchartsChartComponent } from 'highcharts-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutputGraphComponent } from './output-graph/output-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputGraphComponent,
+    HighchartsChartComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/output-graph/output-graph.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-output-graph',
+  templateUrl: './output-graph.component.html',
+  styleUrls: ['./output-graph.component.css']
+})
+export class OutputGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
