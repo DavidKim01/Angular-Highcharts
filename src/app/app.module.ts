404f325464a6238313d818787dc882091cb28144
DavidKim01 <dk9100@gmail.com>
2020-06-02 00:20:12 -0400

    added highcharts import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HighchartsChartComponent } from 'highcharts-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
