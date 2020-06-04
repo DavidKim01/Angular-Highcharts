33ac1b8a6c42d9cb6c0c87a16116b1430c610dd5
DavidKim01 <dk9100@gmail.com>
2020-06-04 12:30:24 -0400

    merge fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { HighchartsChartModule } from 'highcharts-angular';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-=======
-import { HighchartsChartComponent } from 'highcharts-angular';
->>>>>>> master
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
