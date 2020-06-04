684a5c53a5caa8ee62d5519eccbcb0cd54d5554f
DavidKim01 <dk9100@gmail.com>
2020-06-03 23:01:27 -0400

    chart optimizations

--- src/app/output-graph/output-graph.component.ts
+++ src/app/output-graph/output-graph.component.ts
@@ -5,16 +5,16 @@ import HC_export from 'highcharts/modules/exporting';
 import HC_data from 'highcharts/modules/data';
 import HC_drag from 'highcharts/modules/drag-panes';
 import HC_indicators from 'highcharts/indicators/indicators';
-import HC_vbp from 'highcharts/indicators/volume-by-price';
+
 import { HttpClient } from '@angular/common/http';
 import { interval, Subscription } from 'rxjs';
+import { tick } from '@angular/core/testing';
 
 HC_stock(Highcharts);
 HC_export(Highcharts);
 HC_data(Highcharts);
 HC_indicators(Highcharts);
 HC_drag(Highcharts);
-HC_vbp(Highcharts);
 
 declare var require: any;
 let Boost = require('highcharts/modules/boost');
@@ -39,21 +39,54 @@ export class OutputGraphComponent implements OnInit {
     let tickerChoice: string = "AAPL";
 
     Highcharts.getJSON(`${tickerUrl}${tickerChoice}.json?api_key=${apiKey}`, function (data) {
-      let parsedData = data.dataset.data.map((entry) => {
-        return entry.slice(0, 5);
-      });
-      let d = new Date();
-      let n = d.getTime();
-      let altered = parsedData.forEach((entry) => {
-        let dateArray: string[] = entry[0].split('-');
-        let dd: Date = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10) - 1, parseInt(dateArray[2], 10));
-        entry[0] = dd.getTime();
-      })
-      console.log(parsedData);
-      Highcharts.stockChart('container', {
-        
+      // let parsedData: [][] = data.dataset.data.map((entry) => {
+      //   return entry.slice(0, 5);
+      // });
+
+      let parsedData: any[] = [];
+      let volumes: any[] = [];
+      let dataSize: number = data.dataset.data.length;
+
+      let startDate: string = data.dataset.data[dataSize-1][0];
+      let endDate: string = data.dataset.data[0][0];
+
+      let datePulled: Date = new Date();
+      let dateArray: string[] = [];
+      let dateInMs: number = 0;
+
+      for (let i: number = dataSize - 1; i >= 0; i -= 1) {
+        dateArray = data.dataset.data[i][0].split('-');
+        datePulled = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10) - 1, parseInt(dateArray[2], 10));
+        dateInMs = datePulled.getTime();
+        parsedData.push([
+          dateInMs, // the date
+          data.dataset.data[i][1], // open
+          data.dataset.data[i][2], // high
+          data.dataset.data[i][3], // low
+          data.dataset.data[i][4] // close
+        ]);
+
+        volumes.push([
+          dateInMs, // the date
+          data.dataset.data[i][5] // the volume
+        ]);
+      }
+
+      // let formattedDateData = parsedData.forEach((entry) => {
+      //   let dateArray: string[] = entry[0].split('-');
+      //   let dd: Date = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10) - 1, parseInt(dateArray[2], 10));
+      //   entry[0] = dd.getTime();
+      // })
+      // console.log(parsedData);
+
+      Highcharts.stockChart('ohlc-chart', {
+
         title: {
-          text: 'AAPL Stock Price'
+          text: `OHLC Chart (Open-High-Low-Close) for ${tickerChoice}`
+        },
+
+        subtitle: {
+          text: `from ${startDate} to ${endDate}`
         },
 
         rangeSelector: {
@@ -87,11 +120,15 @@ export class OutputGraphComponent implements OnInit {
           selected: 3
         },
 
+        tooltip: {
+          split: true
+        },
+
         series: [{
           type: 'ohlc',
-          id: 'aapl',
-          name: 'AAPL Stock Price',
-          data: parsedData.reverse(),
+          id: 'first',
+          name: `${tickerChoice} Stock Price`,
+          data: parsedData,
           dataGrouping: {
             units: [[
               'day',
@@ -102,8 +139,8 @@ export class OutputGraphComponent implements OnInit {
         {
           type: 'sma',
           name: 'Smart Moving Average 25-day',
-          linkedTo: 'aapl',
-          color: 'blue',
+          linkedTo: 'first',
+          color: 'green',
           dataGrouping: {
             approximation: 'averages',
             groupPixelWidth: 25,
@@ -116,7 +153,7 @@ export class OutputGraphComponent implements OnInit {
         {
           type: 'sma',
           name: 'Smart Moving Average 200-day',
-          linkedTo: 'aapl',
+          linkedTo: 'first',
           color: 'red',
           dataGrouping: {
             approximation: 'averages',
@@ -130,10 +167,14 @@ export class OutputGraphComponent implements OnInit {
         ]
       })
 
-      Highcharts.stockChart('container-2', {
+      Highcharts.stockChart('candle-chart', {
 
         title: {
-          text: 'AAPL Historical'
+          text: `Candlestick Chart for ${tickerChoice} with Volumes in Lower Pane`
+        },
+
+        subtitle: {
+          text: `from ${startDate} to ${endDate}`
         },
 
         rangeSelector: {
@@ -167,10 +208,6 @@ export class OutputGraphComponent implements OnInit {
           selected: 3
         },
 
-        subtitle: {
-          text: 'With SMA and Volume by Price technical indicators'
-        },
-
         yAxis: [{
           startOnTick: false,
           endOnTick: false,
@@ -179,7 +216,7 @@ export class OutputGraphComponent implements OnInit {
             x: -3
           },
           title: {
-            text: 'OHLC'
+            text: 'Candle'
           },
           height: '60%',
           lineWidth: 2,
@@ -206,34 +243,52 @@ export class OutputGraphComponent implements OnInit {
 
         series: [{
           type: 'candlestick',
-          name: 'AAPL',
-          id: 'aapl',
-          zIndex: 2,
-          data: parsedData.reverse()
+          name: `${tickerChoice} Stock Price`,
+          id: 'second',
+          data: parsedData,
+          dataGrouping: {
+            units: [[
+              'day',
+              [1]
+            ]]
+          }
         }, {
           type: 'column',
           name: 'Volume',
           id: 'volume',
-          data: parsedData.reverse(),
+          data: volumes,
+          dataGrouping: {
+            units: [[
+              'day',
+              [1]
+            ]]
+          },
           yAxis: 1
         }, {
-          type: 'vbp',
-          linkedTo: 'aapl',
-          params: {
-            volumeSeriesID: 'volume'
-          },
-          dataLabels: {
-            enabled: false
-          },
-          zoneLines: {
-            enabled: false
+          type: 'sma',
+          name: 'Smart Moving Average 25-day',
+          linkedTo: 'second',
+          color: 'blue',
+          dataGrouping: {
+            approximation: 'averages',
+            groupPixelWidth: 25,
+            units: [[
+              'day',
+              [25]
+            ]]
           }
         }, {
           type: 'sma',
-          linkedTo: 'aapl',
-          zIndex: 1,
-          marker: {
-            enabled: false
+          name: 'Smart Moving Average 200-day',
+          linkedTo: 'second',
+          color: 'orange',
+          dataGrouping: {
+            approximation: 'averages',
+            groupPixelWidth: 200,
+            units: [[
+              'day',
+              [200]
+            ]]
           }
         }]
 
