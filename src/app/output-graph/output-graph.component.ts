33e631d5278abaaa999d2848752badb8edd13f41
DavidKim01 <dk9100@gmail.com>
2020-06-04 18:12:53 -0400

    code refactorings

--- src/app/output-graph/output-graph.component.ts
+++ src/app/output-graph/output-graph.component.ts
@@ -1,33 +1,23 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { interval, Subscription } from 'rxjs';
+import { HttpService } from '../http.service';
+import { Subscription, Observable } from 'rxjs';
+import { timer } from 'rxjs/observable/timer';
 
 import * as Highcharts from 'highcharts'
 import HC_stock from 'highcharts/modules/stock';
 import HC_export from 'highcharts/modules/exporting';
 import HC_data from 'highcharts/modules/data';
-import HC_drag from 'highcharts/modules/drag-panes';
 import HC_indicators from 'highcharts/indicators/indicators';
 import theme from 'highcharts/themes/dark-unica';
-
-
 
 HC_stock(Highcharts);
 HC_export(Highcharts);
 HC_data(Highcharts);
 HC_indicators(Highcharts);
-HC_drag(Highcharts);
 theme(Highcharts);
 
-declare var require: any;
-let Boost = require('highcharts/modules/boost');
-let noData = require('highcharts/modules/no-data-to-display');
-let More = require('highcharts/highcharts-more');
-
-Boost(Highcharts);
-noData(Highcharts);
-More(Highcharts);
-noData(Highcharts);
+const apiKey: string = 'qPkR3WAyaVxXgh6hFAJi';
+const tickerUrl: string = 'https://www.quandl.com/api/v3/datasets/WIKI/';
 
 @Component({
   selector: 'app-output-graph',
@@ -35,275 +25,300 @@ noData(Highcharts);
   styleUrls: ['./output-graph.component.css']
 })
 export class OutputGraphComponent implements OnInit, OnChanges {
-  public apiKey: string = 'qPkR3WAyaVxXgh6hFAJi';
-  public tickerUrl: string = 'https://www.quandl.com/api/v3/datasets/WIKI/';
+  tickers: string[] = ['AAPL', 'IBM', 'C', 'AXP', 'CVS', 'GE', 'MSFT'];
   tickerChoice: string = '';
-  tickers: string[] = ['AAPL','IBM','C','AXP', 'CVS', 'GE', 'MSFT'];
-  
 
-  ngOnInit(): void {
+  subscription: Subscription;
+  source: Observable<number> = timer(0, 120000);
+
+  constructor(private _http: HttpService) { }
+
+  ngOnInit() {
   }
-  ngOnChanges(): void {
+
+  ngOnChanges() {
     this.getCharts(this.tickerChoice);
   }
 
-  getCharts(tickerSelected: string): void {
-    Highcharts.getJSON(`${this.tickerUrl}${tickerSelected}.json?api_key=${this.apiKey}`, function (data) {
-      // let parsedData: [][] = data.dataset.data.map((entry) => {
-      //   return entry.slice(0, 5);
-      // });
+  getCharts(tickerSelected: string) {
 
-      let parsedData: any[] = [];
-      let volumes: any[] = [];
-      let dataSize: number = data.dataset.data.length;
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = this.source.subscribe(val => this._http.getData(`${tickerUrl}${tickerSelected}.json?api_key=${apiKey}`).then(
+      function (data: any) {
+        let timeStamp: Date = new Date();
+        console.log("Fetched new API data at: " + timeStamp.toLocaleTimeString());
 
-      let startDate: string = data.dataset.data[dataSize-1][0];
-      let endDate: string = data.dataset.data[0][0];
+        let parsedData: any[] = [];
+        let volumes: any[] = [];
+        let dataSize: number = data.dataset.data.length;
 
-      let datePulled: Date = new Date();
-      let dateArray: string[] = [];
-      let dateInMs: number = 0;
+        let startDate: string = data.dataset.data[dataSize - 1][0];
+        let endDate: string = data.dataset.data[0][0];
 
-      for (let i: number = dataSize - 1; i >= 0; i -= 1) {
-        dateArray = data.dataset.data[i][0].split('-');
-        datePulled = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10) - 1, parseInt(dateArray[2], 10));
-        dateInMs = datePulled.getTime();
-        parsedData.push([
-          dateInMs, // the date
-          data.dataset.data[i][1], // open
-          data.dataset.data[i][2], // high
-          data.dataset.data[i][3], // low
-          data.dataset.data[i][4] // close
-        ]);
+        let datePulled: Date = new Date();
 
-        volumes.push([
-          dateInMs, // the date
-          data.dataset.data[i][5] // the volume
-        ]);
-      }
+        let dateArray: string[] = [];
+        let dateInMs: number = 0;
 
-      // let formattedDateData = parsedData.forEach((entry) => {
-      //   let dateArray: string[] = entry[0].split('-');
-      //   let dd: Date = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10) - 1, parseInt(dateArray[2], 10));
-      //   entry[0] = dd.getTime();
-      // })
-      // console.log(parsedData);
+        for (let i: number = dataSize - 1; i >= 0; i -= 1) {
+          dateArray = data.dataset.data[i][0].split('-');
+          datePulled = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10) - 1, parseInt(dateArray[2], 10));
+          dateInMs = datePulled.getTime();
 
-      Highcharts.stockChart('ohlc-chart', {
+          parsedData.push([
+            dateInMs,
+            data.dataset.data[i][1],
+            data.dataset.data[i][2],
+            data.dataset.data[i][3],
+            data.dataset.data[i][4]
+          ]);
 
-        title: {
-          text: `OHLC Chart (Open-High-Low-Close) for ${tickerSelected}`
-        },
-
-        subtitle: {
-          text: `from ${startDate} to ${endDate}`
-        },
-
-        rangeSelector: {
-          buttons: [{
-            type: 'week',
-            count: 1,
-            text: '1w'
-          }, {
-            type: 'month',
-            count: 1,
-            text: '1m'
-          }, {
-            type: 'month',
-            count: 3,
-            text: '3m'
-          }, {
-            type: 'month',
-            count: 6,
-            text: '6m'
-          }, {
-            type: 'year',
-            count: 1,
-            text: '1y'
-          }, {
-            type: 'ytd',
-            text: 'YTD'
-          }, {
-            type: 'all',
-            text: 'All'
-          }],
-          selected: 3
-        },
-
-        tooltip: {
-          split: true
-        },
-
-        series: [{
-          type: 'ohlc',
-          id: 'first',
-          name: `${tickerSelected} Stock Price`,
-          data: parsedData,
-          dataGrouping: {
-            units: [[
-              'day',
-              [1]
-            ]]
-          }
-        },
-        {
-          type: 'sma',
-          name: 'Smart Moving Average 25-day',
-          linkedTo: 'first',
-          color: 'green',
-          dataGrouping: {
-            approximation: 'averages',
-            groupPixelWidth: 25,
-            units: [[
-              'day',
-              [25]
-            ]]
-          }
-        },
-        {
-          type: 'sma',
-          name: 'Smart Moving Average 200-day',
-          linkedTo: 'first',
-          color: 'red',
-          dataGrouping: {
-            approximation: 'averages',
-            groupPixelWidth: 200,
-            units: [[
-              'day',
-              [200]
-            ]]
-          }
+          volumes.push([
+            dateInMs,
+            data.dataset.data[i][5]
+          ]);
         }
-        ]
-      })
 
-      Highcharts.stockChart('candle-chart', {
+        Highcharts.stockChart('ohlc-chart', {
 
-        title: {
-          text: `Candlestick Chart for ${tickerSelected} with Volumes in Lower Pane`
-        },
+          title: {
+            text: `OHLC Chart (Open-High-Low-Close) for ${tickerSelected}`
+          },
 
-        subtitle: {
-          text: `from ${startDate} to ${endDate}`
-        },
+          subtitle: {
+            text: `From ${startDate} to ${endDate}, last updated on ${timeStamp}`
+          },
 
-        rangeSelector: {
-          buttons: [{
-            type: 'week',
-            count: 1,
-            text: '1w'
+          rangeSelector: {
+            buttons: [{
+              type: 'week',
+              count: 1,
+              text: '1w'
+            }, {
+              type: 'month',
+              count: 1,
+              text: '1m'
+            }, {
+              type: 'month',
+              count: 3,
+              text: '3m'
+            }, {
+              type: 'month',
+              count: 6,
+              text: '6m'
+            }, {
+              type: 'year',
+              count: 1,
+              text: '1y'
+            }, {
+              type: 'ytd',
+              text: 'YTD'
+            }, {
+              type: 'all',
+              text: 'All'
+            }],
+            selected: 3
+          },
+
+          tooltip: {
+            split: true
+          },
+
+          series: [{
+            type: 'ohlc',
+            id: 'first',
+            name: `${tickerSelected} Stock Price`,
+            data: parsedData,
+            dataGrouping: {
+              units: [[
+                'day',
+                [1]
+              ]]
+            }
+          },
+          {
+            type: 'sma',
+            name: 'Smart Moving Average 25-day',
+            linkedTo: 'first',
+            color: 'green',
+            dataGrouping: {
+              approximation: 'averages',
+              groupPixelWidth: 25,
+              units: [[
+                'day',
+                [25]
+              ]]
+            }
+          },
+          {
+            type: 'sma',
+            name: 'Smart Moving Average 200-day',
+            linkedTo: 'first',
+            color: 'red',
+            dataGrouping: {
+              approximation: 'averages',
+              groupPixelWidth: 200,
+              units: [[
+                'day',
+                [200]
+              ]]
+            }
+          }
+          ]
+        })
+
+        Highcharts.stockChart('candle-chart', {
+
+          title: {
+            text: `Candlestick Chart for ${tickerSelected} with Volumes in Lower Pane`
+          },
+
+          subtitle: {
+            text: `From ${startDate} to ${endDate}, last updated on ${timeStamp}`
+          },
+
+          rangeSelector: {
+            buttons: [{
+              type: 'week',
+              count: 1,
+              text: '1w'
+            }, {
+              type: 'month',
+              count: 1,
+              text: '1m'
+            }, {
+              type: 'month',
+              count: 3,
+              text: '3m'
+            }, {
+              type: 'month',
+              count: 6,
+              text: '6m'
+            }, {
+              type: 'year',
+              count: 1,
+              text: '1y'
+            }, {
+              type: 'ytd',
+              text: 'YTD'
+            }, {
+              type: 'all',
+              text: 'All'
+            }],
+            selected: 3
+          },
+
+          yAxis: [{
+            startOnTick: false,
+            endOnTick: false,
+            labels: {
+              align: 'right',
+              x: -3
+            },
+            title: {
+              text: 'Candle'
+            },
+            height: '60%',
+            lineWidth: 2,
+            resize: {
+              enabled: true
+            }
           }, {
-            type: 'month',
-            count: 1,
-            text: '1m'
-          }, {
-            type: 'month',
-            count: 3,
-            text: '3m'
-          }, {
-            type: 'month',
-            count: 6,
-            text: '6m'
-          }, {
-            type: 'year',
-            count: 1,
-            text: '1y'
-          }, {
-            type: 'ytd',
-            text: 'YTD'
-          }, {
-            type: 'all',
-            text: 'All'
+            labels: {
+              align: 'right',
+              x: -3
+            },
+            title: {
+              text: 'Volume'
+            },
+            top: '65%',
+            height: '35%',
+            offset: 0,
+            lineWidth: 2
           }],
-          selected: 3
-        },
 
-        yAxis: [{
-          startOnTick: false,
-          endOnTick: false,
-          labels: {
-            align: 'right',
-            x: -3
+          tooltip: {
+            split: true
           },
-          title: {
-            text: 'Candle'
-          },
-          height: '60%',
-          lineWidth: 2,
-          resize: {
-            enabled: true
-          }
-        }, {
-          labels: {
-            align: 'right',
-            x: -3
-          },
-          title: {
-            text: 'Volume'
-          },
-          top: '65%',
-          height: '35%',
-          offset: 0,
-          lineWidth: 2
-        }],
 
-        tooltip: {
-          split: true
-        },
+          series: [{
+            type: 'candlestick',
+            name: `${tickerSelected} Stock Price`,
+            id: 'second',
+            data: parsedData,
+            dataGrouping: {
+              units: [[
+                'day',
+                [1]
+              ]]
+            }
+          }, {
+            type: 'column',
+            name: 'Volume',
+            id: 'volume',
+            data: volumes,
+            dataGrouping: {
+              units: [[
+                'day',
+                [1]
+              ]]
+            },
+            yAxis: 1
+          }, {
+            type: 'sma',
+            name: 'Smart Moving Average 25-day',
+            linkedTo: 'second',
+            color: 'blue',
+            dataGrouping: {
+              approximation: 'averages',
+              groupPixelWidth: 25,
+              units: [[
+                'day',
+                [25]
+              ]]
+            }
+          }, {
+            type: 'sma',
+            name: 'Smart Moving Average 200-day',
+            linkedTo: 'second',
+            color: 'orange',
+            dataGrouping: {
+              approximation: 'averages',
+              groupPixelWidth: 200,
+              units: [[
+                'day',
+                [200]
+              ]]
+            }
+          }]
 
-        series: [{
-          type: 'candlestick',
-          name: `${tickerSelected} Stock Price`,
-          id: 'second',
-          data: parsedData,
-          dataGrouping: {
-            units: [[
-              'day',
-              [1]
-            ]]
-          }
-        }, {
-          type: 'column',
-          name: 'Volume',
-          id: 'volume',
-          data: volumes,
-          dataGrouping: {
-            units: [[
-              'day',
-              [1]
-            ]]
-          },
-          yAxis: 1
-        }, {
-          type: 'sma',
-          name: 'Smart Moving Average 25-day',
-          linkedTo: 'second',
-          color: 'blue',
-          dataGrouping: {
-            approximation: 'averages',
-            groupPixelWidth: 25,
-            units: [[
-              'day',
-              [25]
-            ]]
-          }
-        }, {
-          type: 'sma',
-          name: 'Smart Moving Average 200-day',
-          linkedTo: 'second',
-          color: 'orange',
-          dataGrouping: {
-            approximation: 'averages',
-            groupPixelWidth: 200,
-            units: [[
-              'day',
-              [200]
-            ]]
-          }
-        }]
+        });
+      },
+      error => {
+        console.log('Error in getting data...');
+      })
+    );
 
+    // I had initially used the Highcharts.getJSON method to fetch the data but decided to forgo this when adding the 
+    /*
+    Highcharts.getJSON(`${this.tickerUrl}${tickerSelected}.json?api_key=${this.apiKey}`, function (data) {
+      let parsedData: [][] = data.dataset.data.map((entry) => {
+        return entry.slice(0, 5);
       });
+      
+      let formattedDateData = parsedData.forEach((entry) => {
+        let dateArray: string[] = entry[0].split('-');
+        let dd: Date = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10) - 1, parseInt(dateArray[2], 10));
+        entry[0] = dd.getTime();
+      })
+      console.log(parsedData);
+
+
     });
+    */
+
   }
 
 }
